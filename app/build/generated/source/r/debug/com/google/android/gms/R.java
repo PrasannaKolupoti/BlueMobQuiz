45f18099a15c306aa6d871e8b3a50f59a107c0f5
Karthik Verma Jampani <kr510162@dal.ca>
2017-10-31 01:56:59 -0300

    Build changes

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -56,16 +56,16 @@ public final class R {
         public static final int adjust_width = 0x7f080017;
         public static final int auto = 0x7f080020;
         public static final int dark = 0x7f080034;
-        public static final int icon_only = 0x7f08004b;
-        public static final int light = 0x7f08004f;
-        public static final int none = 0x7f080062;
-        public static final int normal = 0x7f080063;
-        public static final int radio = 0x7f080085;
-        public static final int standard = 0x7f0800cc;
-        public static final int text = 0x7f0800e0;
-        public static final int text2 = 0x7f0800e1;
-        public static final int wide = 0x7f0800ee;
-        public static final int wrap_content = 0x7f0800f1;
+        public static final int icon_only = 0x7f08004a;
+        public static final int light = 0x7f08004e;
+        public static final int none = 0x7f080061;
+        public static final int normal = 0x7f080062;
+        public static final int radio = 0x7f080084;
+        public static final int standard = 0x7f0800cb;
+        public static final int text = 0x7f0800df;
+        public static final int text2 = 0x7f0800e0;
+        public static final int wide = 0x7f0800ed;
+        public static final int wrap_content = 0x7f0800f0;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f090003;
