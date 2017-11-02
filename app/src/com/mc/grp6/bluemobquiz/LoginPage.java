62a34612e4abf97c701a8963667ec4935e674fdc
Karthik Verma Jampani <kr510162@dal.ca>
2017-11-02 01:08:25 -0300

    build files

--- app/src/com/mc/grp6/bluemobquiz/LoginPage.java
+++ app/src/com/mc/grp6/bluemobquiz/LoginPage.java
@@ -1,7 +1,6 @@
 package com.mc.grp6.bluemobquiz;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
