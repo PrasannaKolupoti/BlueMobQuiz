a17be7b7fc5e94589f6bcb548d16cea299cf791c
Karthik Verma Jampani <kr510162@dal.ca>
2017-11-26 03:10:35 -0400

    Professor bluetooth discovery

--- app/src/com/mc/grp6/bluemobquiz/ProfessorHome.java
+++ app/src/com/mc/grp6/bluemobquiz/ProfessorHome.java
@@ -1,11 +1,16 @@
 package com.mc.grp6.bluemobquiz;
 
 import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -38,6 +43,8 @@ public class ProfessorHome extends SalesforceActivity {
     public ArrayList<String> quizIDList = new ArrayList<String>();
     public ArrayList<String> quizNameList = new ArrayList<String>();
     public ArrayList<String> quizStatusList = new ArrayList<String>();
+    BluetoothAdapter mBluetoothAdapter;
+    private static final String TAG = "ProfessorHome";
 
     @Override
     public void onResume() {
@@ -64,6 +71,7 @@ public class ProfessorHome extends SalesforceActivity {
         setContentView(R.layout.activity_professor_home);
         userID = getIntent().getExtras().getString("userID");
         gestureObject = new GestureDetectorCompat(this, new CustomGesture());
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         Button createQuizButton = (Button) findViewById(R.id.createQuizButton);
         ListView quizList = (ListView) findViewById(R.id.quizListView);
         quizList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -84,7 +92,11 @@ public class ProfessorHome extends SalesforceActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 String quizID = quizIDList.get(position);
                 String isActive  = quizStatusList.get(position) ;
-
+                if(!mBluetoothAdapter.isEnabled()) {
+                    enableDisableBT();
+                    checkBTPermissions();
+                    makeBtDiscoverable(view);
+                }
                 if(isActive.equals("false")){
                     String quizStatus = "shared";
                     AlertDialog.Builder builder;
@@ -152,6 +164,100 @@ public class ProfessorHome extends SalesforceActivity {
                 startActivity(intent);
             }
         });
+    }
+    public void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(mBroadcastReceiver1);
+        unregisterReceiver(mBroadcastReceiver2);
+    }
+    // This method is required for all devices running API23+. Android must programmatically check the permissions for bluetooth. Putting the proper permissions in the manifest is not enough.
+    private void checkBTPermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
+                this.requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+            }
+        }else{
+        }
+    }
+    public void makeBtDiscoverable(View view) {
+        Log.d(TAG, "Enable_Discoverable: Making device discoverable for 600 seconds.");
+
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 600);
+        startActivity(discoverableIntent);
+
+        IntentFilter intentFilter = new IntentFilter(mBluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+        registerReceiver(mBroadcastReceiver2,intentFilter);
+
+    }
+    /**
+     * Broadcast Receiver for changes made to bluetooth states such as:
+     * 1) Discoverability mode on/off or expire.
+     */
+    private final BroadcastReceiver mBroadcastReceiver2 = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)) {
+
+                int mode = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR);
+
+                switch (mode) {
+                    //Device is in Discoverable Mode
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
+                        Log.d(TAG, "mBroadcastReceiver2: Discoverability Enabled.");
+                        break;
+                    //Device not in discoverable mode
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
+                        Log.d(TAG, "mBroadcastReceiver2: Discoverability Disabled. Able to receive connections.");
+                        break;
+                    case BluetoothAdapter.SCAN_MODE_NONE:
+                        Log.d(TAG, "mBroadcastReceiver2: Discoverability Disabled. Not able to receive connections.");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTING:
+                        Log.d(TAG, "mBroadcastReceiver2: Connecting....");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTED:
+                        Log.d(TAG, "mBroadcastReceiver2: Connected.");
+                        break;
+                }
+
+            }
+        }
+    };
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
+                switch(state){
+                    case BluetoothAdapter.STATE_OFF:
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        break;
+                }
+            }
+        }
+    };
+    public void enableDisableBT(){
+        if(mBluetoothAdapter == null){
+        }
+        if(!mBluetoothAdapter.isEnabled()){
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1, BTIntent);
+        }
     }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
